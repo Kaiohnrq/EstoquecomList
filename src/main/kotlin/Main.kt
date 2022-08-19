7b40e8ab0cf377e7d95e633d956d46e5ed77441f
Kaio <kaio.gunz@gmail.com>
2022-08-19 14:23:52 -0300

    Menu melhorado e opção de continuar adicionando itens sem voltar ao menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,27 +7,5 @@ fun main() {
     //Remover dados da list;
     //Atualizar dados da list.
     //Apresentar todos os dados da list.
-    while (true) {
-        println("1 - Adicionar item ao estoque: ")
-        println("2 - Remover item do estoque: ")
-        println("3 - Atualizar item no estoque: ")
-        println("4 - Mostrar itens no estoque: ")
-        println("5 - Sair: ")
-        println()
-        println("Opção: ")
-
-        when (readln().toInt()) {
-            1 -> {
-                println("Digite um produto para adicionar")
-                val produto = readln()
-                addItem(produto)
-            }
-            2 -> removeItem()
-            3 -> alteraItem()
-            4 -> exibeItem()
-            5 -> break
-            else -> println("Opção selecionada inválida! \n")
-
-        }
-    }
+    exibeMenu()
 }
--- src/main/kotlin/function.kt
+++ src/main/kotlin/function.kt
@@ -1,10 +1,26 @@
 val itens = mutableListOf<String>("Playstation 4", "Playstation 5")
 
-fun addItem(produto: String){
+fun addItem(){
+    println("Digite o produto que deseja adicionar: ")
+    val produto = readln()
+
     if(produto != ""){
         itens.add(produto)
         println("Produto $produto adicionado com sucesso!")
         println()
+        println("Deseja adicionar outro produto?")
+        println("1 - Sim!")
+        println("2 - Não! Voltar ao menu")
+        val resp = readln().toInt()
+
+        if(resp == 1){
+            addItem()
+        } else if(resp == 2){
+            exibeMenu()
+        } else {
+            println("Opção selecionada invalida!")
+        }
+
     } else{
         println("Produto invalido!")
         println()
@@ -46,5 +62,27 @@ fun exibeItem(){
     println()
     println("Produtos em estoque: $itens")
     println()
+}
+
+fun exibeMenu(){
+    while (true) {
+        println("1 - Adicionar item ao estoque: ")
+        println("2 - Remover item do estoque: ")
+        println("3 - Atualizar item no estoque: ")
+        println("4 - Mostrar itens no estoque: ")
+        println("5 - Sair: ")
+        println()
+        println("Opção: ")
+
+        when (readln().toInt()) {
+            1 -> addItem()
+            2 -> removeItem()
+            3 -> alteraItem()
+            4 -> exibeItem()
+            5 -> break
+            else -> println("Opção selecionada inválida! \n")
+
+        }
+    }
 }
 
